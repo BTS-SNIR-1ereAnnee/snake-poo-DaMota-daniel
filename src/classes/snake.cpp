dd3e3a3c76e54b8e34cc582c031c393b29b61e2f
DaMota-daniel <da-mota.daniel@lyceeppr.fr>
2020-03-04 10:47:36 +0100

    implémentation des fonction de mouvement du serpent

--- src/classes/snake.cpp
+++ src/classes/snake.cpp
@@ -9,29 +9,71 @@ using namespace std ;
 snake::snake()
 {
 
+	for(int x = 0; x < longeur; x++)
+	{
+		Points[x].setPoint(0,x);
+	
+	}
+
 }
 
 
 snake::snake(int x,int y)
 {
+	for(int d = 0; d < longeur; d++)
+	{
+		Points[d].setPoint(x,d+y);
+		
+	}
 
 }
 
 void snake::moveDown()
 {
-    
+	Points[0].moveDown();
+	
+ 	for(int d = 1; d < longeur; d++)
+	{
+		Points[d]= Points[d-1];
+		
+	} 
 }
 
 void snake::moveUp()
 {
-    
+	
+    Points[0].moveUp();
+	
+ 	for(int d = 1; d < longeur; d++)
+	{
+		Points[d]= Points[d-1];
+		
+	} 
 }
 
 void snake::moveRight()
 {
-   
+   Points[0].moveRight();
+	
+ 	for(int d = 1; d < longeur; d++)
+	{
+		Points[d]= Points[d-1];
+		
+	} 
 }
 void snake::moveLeft()
 {
-    
+    Points[0].moveLeft();
+	
+ 	for(int d = 1; d < longeur; d++)
+	{
+		Points[d]= Points[d-1];
+		
+	} 
 }
+
+void snake::mangerPomme()
+{
+	longeur = longeur + 1;
+}
+
--- src/classes/snake.h
+++ src/classes/snake.h
@@ -31,7 +31,8 @@ public:
         Move point up
     */
     void moveRight();
-
+	
+	void mangerPomme();
 
 private: 
 	
